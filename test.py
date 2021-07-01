2a60f7946d303cc3952e74f6368acd987f648799
jtsy83 <82754634+jtsy83@users.noreply.github.com>
2021-07-01 11:21:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+# errors in code: indent on line 5, '_' not allowed in var name
+
+
 print("Hello, world!")
 
-print("What is your name?")
+    print("What is your name?")
 
-myName = input()
+my_Name = input()
 
-print("It is good to meet you, " + myName)
+print("It is good to meet you, " + my_Name)
