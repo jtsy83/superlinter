b6a38df215f8c2834b8745b5a5d4c9981c50a73b
jtsy83 <82754634+jtsy83@users.noreply.github.com>
2021-07-01 11:31:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
-# errors in code: indent on line 5, '_' not allowed in var name
+# errors in code: 
+# line 10: no indent
+# line 12, 14: '_' not allowed at start of var
+# line 20: 'myage' var not defined
 
+# CODE START #
 
-print("Hello, world!")
+print("Hello world!")
 
     print("What is your name?")
 
-my_Name = input()
+_myName = input()
 
-print("It is good to meet you, " + my_Name)
+print("It is good to meet you, " + _myName)
+
+print("What is your age?")
+
+myAge = input()
+
+print("Your age is " + myage)
